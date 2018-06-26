bbc384acd4464b5c921c27845b21448165125d6f
René Meyer <36350532+BuggerSee@users.noreply.github.com>
2018-06-26 14:18:16 +0200

    Removed GPU port vector, replaced with GPU start port

--- src/constants.rs
+++ src/constants.rs
@@ -11,7 +11,7 @@ pub static FAIL_COLOR: &'static str = "red";
 pub static WALTON_DATA_COLOR: &'static str = "cyan";
 pub static MING_DATA_COLOR: &'static str = "purple";
 pub static AMOUNT_GPU: &'static i32 = &1;
-pub static PORT_NUMBER_VECTOR: &'static [&'static str;7] = &["12140","12126","12127","12128","12129","12130","12131"];
+pub static PORT_NUMBER_START: &'static i32 = &12140;
 
 pub fn print_color(input: &str, color: &String) {
     if color.contains("red") {
@@ -32,4 +32,41 @@ pub fn print_color(input: &str, color: &String) {
 pub fn print_block(mut slice: &[u8]) {
     let num = slice.read_u32::<BigEndian>().unwrap();
     print_color(&format!("  BlockNumber: {}", num), &SUCCESS_COLOR.to_owned());
+}
+
+pub fn print_44(packets: &Vec<u8>) {
+    print_color("Formatted:", &MING_DATA_COLOR.to_owned());
+    let unidentified_1 = packets.get(0..2).unwrap();
+    let unidentified_2 = packets.get(2..4).unwrap();
+    let input = packets.get(4..36).unwrap();
+    let input_nonce = packets.get(36..44).unwrap();
+    print_color(&format!("  Unidentified_1: {:?}", &unidentified_1.to_vec()),
+                &STANDARD_COLOR.to_owned());
+    print_color(&format!("  Unidentified_2: {:?}", &unidentified_2.to_vec()),
+                &STANDARD_COLOR.to_owned());
+    print_color(&format!("  Input Value   : {:?}", &input.to_vec()),
+                &STANDARD_COLOR.to_owned());
+    print_color(&format!("  Input Nonce   : {:?}", &input_nonce.to_vec()),
+                &STANDARD_COLOR.to_owned());
+}
+
+pub fn print_96(packets: &Vec<u8>) {
+    print_color("Formatted:", &WALTON_DATA_COLOR.to_owned());
+    let block_number = packets.get(1..5).unwrap();  //Byte Index 1-4 - Index 0 = set/stop
+    let count = packets.get(77..85).unwrap(); // Count is constant
+    let input_nonce = packets.get(37..45).unwrap();
+    let algtion_val = packets.get(85..96).unwrap();
+    let input_val = packets.get(5..37).unwrap();
+    let target_val = packets.get(45..77).unwrap();
+    print_block(&block_number);
+    print_color(&format!("  Count Val  : {:?}", &count.to_vec()),
+                &SUCCESS_COLOR.to_owned());
+    print_color(&format!("  Input Nonce: {:?}", &input_nonce.to_vec()),
+                &SUCCESS_COLOR.to_owned());
+    print_color(&format!("  Algtion Val: {:?}", &algtion_val.to_vec()),
+                &SUCCESS_COLOR.to_owned());
+    print_color(&format!("  Input   Val: {:?}", &input_val.to_vec()),
+                &SUCCESS_COLOR.to_owned());
+    print_color(&format!("  Target  Val: {:?}", &target_val.to_vec()),
+                &SUCCESS_COLOR.to_owned());
 }
